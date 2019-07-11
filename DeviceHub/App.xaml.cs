2696a5ff056f1132b5bb2eb6131e10fe275b3aa0
Rick Burke <astroman133@gmail.com>
2019-07-11 14:55:40 -0700

    Initial Commit

---
+++ DeviceHub/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace ASCOM.DeviceHub
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup( StartupEventArgs e )
+		{
+			base.OnStartup( e );
+
+			try
+			{
+				// Launch the ASCOM Local Server
+
+				Server.Startup( e.Args );
+			}
+			catch ( Exception xcp )
+			{
+				string msg = String.Format( "DeviceHub caught a fatal exception during startup\r\n{0}", xcp.Message );
+				MessageBox.Show( msg, "DeviceHub", MessageBoxButton.OK, MessageBoxImage.Stop );
+
+				if ( Application.Current != null )
+				{
+					Application.Current.Shutdown();
+				}
+			}
+		}
+    }
+}
---
+++ ProductAssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. 
+
+// Changing the AssemblyCompany and the AssemblyProduct will change the location and name
+// of the application settings xml file.
+
+[assembly: AssemblyCompany( "The ASCOM Initiative" )]
+[assembly: AssemblyCopyright( "Copyright ©  2019" )]
+[assembly: AssemblyTrademark( "" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "6.4.1.0" )]
+[assembly: AssemblyFileVersion( "6.4.1.0" )]
